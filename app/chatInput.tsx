7e1510be1417a3e727d19878f1bbcd2b94344b68
Mehran Rasheed <engr.mehranrasheed@gmail.com>
2023-09-23 19:14:48 +0500

    Added enter button.

--- app/chatInput.tsx
+++ app/chatInput.tsx
@@ -56,9 +56,25 @@ const ChatInput = ({ onSend, disabled }: InputProps) => {
       )}
       {!disabled && (
         <button
+          className="inline-flex items-center justify-center px-4 py-2 bg-blue-500 text-white rounded shadow hover:bg-blue-700"
           onClick={() => sentInput()}
-          className="p-2 rounded-md text-gray-500 bottom-1.5 right-1"
-        ></button>
+        >
+          <svg
+            className="w-4 h-4"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M12 18h.01M8 12h8M19 6v4m-3 3v4m-3 3v4"
+            ></path>
+          </svg>
+          enter
+        </button>
       )}
     </div>
   );
